3fb6d5eb4ee9f17aa7bb81b91ff5ffb3c7666e5a
jairpro <jairedias@yahoo.com.br>
2020-10-19 01:55:21 -0300

    Configura para deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from 'express'
 import path from 'path'
 import 'express-async-errors'
@@ -16,5 +17,5 @@ app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
 
-app.listen(process.env.SERVER_PORT)
+app.listen(process.env.PORT || 3333)
  
